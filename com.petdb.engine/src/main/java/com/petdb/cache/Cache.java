aa4cd1534ea93909d994efd9c731826b087fee94
EMOS <emosdevelop@gmail.com>
2020-11-07 19:14:35 +0100

    refactoring

--- com.petdb.engine/src/main/java/com/petdb/cache/Cache.java
+++ com.petdb.engine/src/main/java/com/petdb/cache/Cache.java
@@ -10,19 +10,19 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public final class Cache {
 
-    private final static ConcurrentHashMap<Key, Value> store = new ConcurrentHashMap<>();
+    private final static ConcurrentHashMap<Key, Value> STORE = new ConcurrentHashMap<>();
 
     public Cache() {
         //TODO load from disk
     }
 
     public String set(Key key, Value value) {
-        Cache.store.put(key, value);
+        Cache.STORE.put(key, value);
         return "OK";
     }
 
     public String get(Key key) {
-        var value = Cache.store.get(key);
+        var value = Cache.STORE.get(key);
         if (value == null) {
             return String.format("Key = %s, not set", key.getData());
         }
@@ -30,24 +30,24 @@ public final class Cache {
     }
 
     public String delete(Key key) {
-        Cache.store.remove(key);
+        Cache.STORE.remove(key);
         return "DELETED";
     }
 
     public String commit(Transaction transaction) {
-        Cache.store.putAll(transaction.getMap());
+        Cache.STORE.putAll(transaction.getMap());
         return String.format("COMMIT: %s", transaction.getUuid());
     }
 
     public int count() {
-        return Cache.store.size();
+        return Cache.STORE.size();
     }
 
     public Map<Key, Value> getStore() {
-        return Collections.unmodifiableMap(Cache.store);
+        return Collections.unmodifiableMap(Cache.STORE);
     }
 
     public void clear() {
-        Cache.store.clear();
+        Cache.STORE.clear();
     }
 }
