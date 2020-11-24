4a057717ea61dbc79a227e704e2b3e25c79476b8
EMOS <emosdevelop@gmail.com>
2020-11-24 23:08:21 +0100

    added ASCII banner art at startup

--- com.petdb.server/src/main/java/com/petdb/driver/Driver.java
+++ com.petdb.server/src/main/java/com/petdb/driver/Driver.java
@@ -2,7 +2,9 @@ package com.petdb.driver;
 
 import com.petdb.server.Server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.logging.Logger;
 
 public final class Driver {
@@ -10,6 +12,14 @@ public final class Driver {
     private final static Logger LOGGER = Logger.getLogger(Driver.class.getName());
 
     public static void main(String[] args) {
+        try (var reader = new BufferedReader(
+                new InputStreamReader(
+                        Driver.class.getResourceAsStream("/petdb-banner.txt")
+                ))) {
+            reader.lines().forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         try {
             int port = 0;
             if (args.length != 1) {
