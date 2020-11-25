1c7c678a73c852017eb05cae1d6f69f9877fa56b
EMOS <emosdevelop@gmail.com>
2020-11-25 14:29:37 +0100

    refactoring

--- com.petdb.server/src/main/java/com/petdb/driver/Driver.java
+++ com.petdb.server/src/main/java/com/petdb/driver/Driver.java
@@ -10,23 +10,28 @@ import java.util.logging.Logger;
 public final class Driver {
 
     private final static Logger LOGGER = Logger.getLogger(Driver.class.getName());
+    private final static int DEFAULT_PORT = 12542;
 
     public static void main(String[] args) {
         printBanner();
+        int port = getPort(args);
         try {
-            int port = 0;
-            if (args.length != 1) {
-                port = 12542;
-                LOGGER.info("Default server start up");
-            } else {
-                port = Integer.parseInt(args[0]);
-            }
-            LOGGER.info(String.format("Working directory = %s", System.getProperty("user.dir")));
-            LOGGER.info(String.format("Port = %d", port));
             new Server(port).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static int getPort(String[] args) {
+        int port = 0;
+        if (args.length != 1) {
+            LOGGER.info(String.format("Default port = %d", DEFAULT_PORT));
+            return DEFAULT_PORT;
+        } else {
+            port = Integer.parseInt(args[0]);
+        }
+        LOGGER.info(String.format("Port = %d", port));
+        return port;
     }
 
     private static void printBanner() {
